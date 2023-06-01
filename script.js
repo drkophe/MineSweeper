93d3cb23c21175fee1aa569acc2e9552a4403ee1
kophe <lorenzo.bosi.pro@gmail.com>
2023-06-01 15:54:00 +0200

    base

---
+++ script.js
@@ -0,0 +1,31 @@
+// Comment se déroule le jeux : 
+// 1) Quand j'appuie sur une difficultés : un tableau correspondant a la difficulté apparait
+// 2) Le tableau est cacher, quand je clique sur une case : dévoile une partie du tableau
+// 3) Les nombre m'indique le nombre de mine qu'il y a autour de la case en question
+// 4) Quand je clique sur une case avec numéro dévoile la case
+// 5) Quand je clique sur une case sans aucun numéro : dévoile toute les cases adjacents jusqua etre bloquer par un némro
+// 6) Quand je clique sur une bombe : game over
+// 7) Quand je clique droit sur une case non dévoilé : ajoute un drapeau
+// 8) Quand je clique droit sur une case avec un drapeau : ajouter un point d'interogation
+// 9) Quand je clique droit sur une case avec un point d'intérogation : redevient une case non devoilé classique
+
+// COOK TIME :
+// Définir un nombre de bombe a posé
+// Parcourir le tableau
+// A chaque
+
+
+// VARIABLE //
+let tabGame = document.querySelector('main');
+console.log(tabGame);
+
+// FUNCTION //
+
+
+
+// ACTION //
+for(x=0; x>10; x++){
+    let row = document.querySelector('row_' + x);
+    console.log(row);
+}
+
